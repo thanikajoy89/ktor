554a3bdd02931869268cf04e7ba61a5b1a78abab
anastasiia.spaseeva <anastasiiia.spaseeva@jetbrains.com>
2021-11-08 20:51:16 +0100

    Save heap dump on OOM for jvmTest

--- buildSrc/src/main/kotlin/JvmConfig.kt
+++ buildSrc/src/main/kotlin/JvmConfig.kt
@@ -69,6 +69,7 @@ fun Project.configureJvm() {
     val jvmTest: KotlinJvmTest = tasks.getByName<KotlinJvmTest>("jvmTest") {
         ignoreFailures = true
         maxHeapSize = "2g"
+        jvmArgs("-XX:+HeapDumpOnOutOfMemoryError")
         exclude("**/*StressTest *")
     }
 
@@ -78,7 +79,7 @@ fun Project.configureJvm() {
 
         ignoreFailures = true
         maxHeapSize = "2g"
-        jvmArgs("-XX:+HeapDumpOnOutOfMemoryError")
+        jvmArgs("""-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./oom_dump.hprof""")
         setForkEvery(1)
         systemProperty("enable.stress.tests", "true")
         include("**/*StressTest*")
