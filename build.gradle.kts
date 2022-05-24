3efcc1a482a4d88181e1681c33929e166c6d779c
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2022-05-25 00:28:03 +0300

    Force kotlin dependencies to use the same version

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,6 +101,8 @@ plugins {
     id("com.osacky.doctor") version "0.8.1"
 }
 
+val kotlinVersion = libs.versions.kotlin.version.get()
+
 allprojects {
     group = "io.ktor"
     version = configuredVersion
@@ -129,6 +131,16 @@ allprojects {
 
     configurations {
         maybeCreate("testOutput")
+    }
+
+    configurations.configureEach {
+        if (isCanBeResolved) {
+            resolutionStrategy.eachDependency {
+                if (requested.group == "org.jetbrains.kotlin") {
+                    useVersion(kotlinVersion)
+                }
+            }
+        }
     }
 
     kotlin {
