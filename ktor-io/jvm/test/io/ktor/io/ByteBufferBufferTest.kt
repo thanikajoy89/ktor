61aa564b75d3af74a4b1d3d42cb9d03f42834579
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2023-04-07 11:04:34 +0200

    Extract Buffer tests to the separate suite

---
+++ ktor-io/jvm/test/io/ktor/io/ByteBufferBufferTest.kt
@@ -0,0 +1,4 @@
+package io.ktor.io
+
+class ByteBufferBufferTest {
+}
---
+++ ktor-io/ktor-io-testing/build.gradle.kts
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("test"))
+
+                implementation(project(":ktor-io"))
+            }
+        }
+    }
+}
