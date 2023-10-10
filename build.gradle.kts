83e1d0b52610aef58283023630b6e729b6134c9f
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2023-10-10 13:51:24 +0200

    Disable osacky.doctor

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ plugins {
     id("org.jetbrains.dokka") version "1.7.20" apply false
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.2"
     id("kotlinx-atomicfu") version "0.22.0-wasm1" apply false
-    id("com.osacky.doctor") version "0.8.1"
+    //id("com.osacky.doctor") version "0.8.1"
 }
 
 allprojects {
